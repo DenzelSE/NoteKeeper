7ab298216bfc77d4f335e879a5d25f3a97d8d398
dselokela023 <dselokela023@student.wethinkco.de>
2024-06-06 11:48:30 +0200

    creating frames for the app. textarea added

---
+++ src/project/Frame.java
@@ -0,0 +1,29 @@
+package project;
+import javax.swing.*;
+
+public class Frame {
+    
+    public static JFrame frame;
+    public static JTextArea textArea;
+
+    public static void main(String[] args) {
+        createFrame();
+        createTextarea();
+
+        frame.setVisible(true);
+        
+    }
+
+    public static void createFrame(){
+        frame = new JFrame("Note Keeper");
+
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void createTextarea() {
+        
+        textArea = new JTextArea();
+        frame.add(textArea);
+    }
+}
